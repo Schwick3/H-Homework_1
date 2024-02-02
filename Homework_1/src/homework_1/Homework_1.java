a9afc493f28c47c92a02af2763b62a80963815f7
skylarperry <skylarperry@skylars-air-3>
2024-02-02 10:29:32 -0600

    First commit

---
+++ Homework_1/src/homework_1/Homework_1.java
@@ -0,0 +1,14 @@
+package homework_1;
+
+public class Homework_1 {
+
+	public static void main(String[] args) {
+		System.out.println("Skylar Perry: Schwick3");
+		System.out.println("Henry Clark: hpc773");
+		System.out.println("Kyle Blank: kcbhgh");
+		System.out.println("Karsyn Williams: karsyngw");
+
+
+	}
+
+}
